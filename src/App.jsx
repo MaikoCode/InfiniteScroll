a1bff31fa3b3ab0608cba5111647d98d1fdc9512
Maikoke <mamadoumaikoke11@gmail.com>
2023-03-22 07:25:26 +0000

    deployment

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from 'react'
+import InfiniteScroll from './Components/InfiniteScroll/InfiniteScroll'
+
+function App() {
+ 
+  // Access key: 1TTrCXGk0lb7ACbQSmsHnL9y8avJ0ww1MR-bzrxZ91s
+  // Secret Key: SNG2b6KjyNpo72MRB7Y098UjiHQf_PLRJjsUHEY-N64
+
+  return (
+    <div className="App">
+      <InfiniteScroll />
+    </div>
+  )
+}
+
+export default App
